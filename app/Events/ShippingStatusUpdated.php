427e3866a2dd4eeae054ed20cf89ff07133c21d8
Janzell Jurilla <janzell1989@gmail.com>
2017-11-08 23:09:08 +0800

    initial commit

---
+++ app/Events/ShippingStatusUpdated.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ShippingStatusUpdated implements ShouldBroadcast
+{
+    /**
+     * Information about the shipping status update.
+     *
+     * @var string
+     */
+    public $update;
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('order.1');
+    }
+}
